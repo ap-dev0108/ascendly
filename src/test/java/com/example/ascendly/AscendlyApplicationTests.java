be4d2193bd1f174b0607b1ca2ef242a2892346bb
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-21 15:12:44 +0545

    Project Initiation

---
+++ src/test/java/com/example/ascendly/AscendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ascendly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
