0ba0a8507351c7cf7418f572d662fa5a43fd92ed
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-22 11:52:09 +0545

    Creating default user entity

---
+++ src/main/java/com/example/worklens/entity/User.java
@@ -0,0 +1,56 @@
+package com.example.worklens.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @GeneratedValue @Id
+    public Long user_id;
+
+    @Column(name = "full_name")
+    public String full_name;
+
+    @Column(name = "email")
+    public String email;
+
+    @Column(name = "password")
+    public String password;
+
+    public User(Long id, String name, String email, String password) {
+        this.user_id = id;
+        this.full_name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    //GETTER
+    public Long getUser_id() {
+        return user_id;
+    }
+    public String getFull_name() {
+        return full_name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    //SETTER
+    public void setUser_id(Long id) {
+        this.user_id = id;
+    }
+    public void setFull_name(String name) {
+        this.full_name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
