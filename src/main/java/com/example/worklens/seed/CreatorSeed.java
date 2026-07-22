23cd0f18d8f0c761e184245a419efff3eb9db6f2
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-22 12:02:55 +0545

    Seeding creator data

--- src/main/java/com/example/worklens/seed/CreatorSeed.java
+++ src/main/java/com/example/worklens/seed/CreatorSeed.java
@@ -1,4 +1,31 @@
 package com.example.worklens.seed;
 
-public class CreatorSeed {
+import com.example.worklens.entity.User;
+import com.example.worklens.repository.CreatorRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class CreatorSeed implements CommandLineRunner {
+    private final CreatorRepository _creator;
+    private final PasswordEncoder _password;
+
+    public CreatorSeed(CreatorRepository creator, PasswordEncoder password) {
+        this._creator = creator;
+        this._password = password;
+    }
+
+    @Override
+    public void run(String...args) {
+        if(!_creator.existsByEmail("aryan@worklens.com")) {
+            User creator = new User();
+
+            creator.setEmail("aryan@worklens.com");
+            creator.setFull_name("Aryan");
+            creator.setPassword(_password.encode("aryan@worklens"));
+
+            _creator.save(creator);
+
+            System.out.println("Creator account seeded");
+        }
+    }
 }
