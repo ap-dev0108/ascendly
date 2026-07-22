1cd63424a7751dce98920ff70e999df3089f3ac3
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-22 11:41:47 +0545

    Initial restart

---
+++ src/test/java/com/example/worklens/WorklensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worklens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorklensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
