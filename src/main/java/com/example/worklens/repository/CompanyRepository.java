7589f31e4089db456b35e6f3fcb1ea85bbf8f2d8
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-24 11:38:53 +0545

    chore(interface): Extending company repo to JpaRepository

--- src/main/java/com/example/worklens/repository/CompanyRepository.java
+++ src/main/java/com/example/worklens/repository/CompanyRepository.java
@@ -3,6 +3,8 @@ package com.example.worklens.repository;
 import com.example.worklens.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
 }
