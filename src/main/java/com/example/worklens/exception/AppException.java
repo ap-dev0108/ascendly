88ae1320416dd59619009961df0623d5d8561953
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-24 11:53:48 +0545

    chore(exception): Creating a base file that sets messages and statuses

--- src/main/java/com/example/worklens/exception/AppException.java
+++ src/main/java/com/example/worklens/exception/AppException.java
@@ -1,7 +1,16 @@
 package com.example.worklens.exception;
 
+import org.springframework.http.HttpStatus;
+
 public abstract class AppException extends RuntimeException{
-    public AppException(String message) {
+    private final HttpStatus status;
+
+    public AppException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
