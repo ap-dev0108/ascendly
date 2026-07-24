ac8fe7443ef2e0bbaa971a30804e416b72c951fb
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-24 11:37:51 +0545

    chore(services): Implementing logic while completing the contract

--- src/main/java/com/example/worklens/services/CompanyServices.java
+++ src/main/java/com/example/worklens/services/CompanyServices.java
@@ -1,24 +1,43 @@
 package com.example.worklens.services;
 
 import com.example.worklens.entity.Company;
+import com.example.worklens.exception.custom.ResourceNotFoundException;
 import com.example.worklens.repository.CompanyRepository;
+import com.example.worklens.services.interfaces.CompanyImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CompanyServices {
-    private final CompanyRepository _company;
+public class CompanyServices implements CompanyImpl {
+    private final CompanyRepository companyrepo;
 
     public CompanyServices(CompanyRepository company) {
-        this._company = company;
+        this.companyrepo = company;
     }
 
-    public List<Company> GetAllCompanies() {
-        var company = _company.findAll();
-        if (company.isEmpty()) {
-            return null;
-        }
+    @Override
+    public List<Company> getAllCompanies() {
+        List<Company> company = companyrepo.findAll();
         return company;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyrepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Company with ID: "+ id + " not found"));
+    }
+
+    @Override
+    public Company saveCompany(Company company) {
+        return companyrepo.save(company);
+    }
+
+    @Override
+    public void deleteCompanyById(Long id) {
+        companyrepo.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Company with ID: "+id+" cannot be found"));
+        companyrepo.deleteById(id);
     }
 }
