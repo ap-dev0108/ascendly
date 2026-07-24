51c8169a184302f4e61ad208aa9073a7091c4a97
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-24 09:41:13 +0545

    chore(exception): Creating global exception handler

---
+++ src/main/java/com/example/worklens/exception/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.example.worklens.exception;
+
+import com.example.worklens.exception.custom.DuplicateResourceException;
+import com.example.worklens.exception.custom.ForbiddenAccessException;
+import com.example.worklens.exception.custom.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleNotFound(ResourceNotFoundException ex) {
+        return build(ex.getStatus(), ex.getMessage());
+    }
+
+    @ExceptionHandler(ForbiddenAccessException.class)
+    public ResponseEntity<ExceptionResponse> handleForbidden(ForbiddenAccessException ex) {
+        return build(ex.getStatus(), ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ExceptionResponse> handleDuplicate(DuplicateResourceException ex) {
+        return build(ex.getStatus(), ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> InternalServerException(Exception ex) {
+        return build(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
+
+    private ResponseEntity<ExceptionResponse> build(HttpStatus status, String message) {
+        return ResponseEntity
+                .status(status)
+                .body(new ExceptionResponse(
+                    message,
+                    status.value(),
+                    LocalDateTime.now()
+        ));
+    }
+}
--- src/main/java/com/example/worklens/exception/custom/InternalServerError.java
+++
@@ -1,9 +0,0 @@
-package com.example.worklens.exception.custom;
-
-import com.example.worklens.exception.AppException;
-
-public class InternalServerError extends AppException {
-    public InternalServerError(String message) {
-        super(message);
-    }
-}
