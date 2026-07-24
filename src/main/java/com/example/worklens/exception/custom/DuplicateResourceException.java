5da0514eeda42cb00d116b409fe3e462d863e519
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-24 11:55:59 +0545

    fix(custom exception): Updating the super() with respective HTTP Statuses

--- src/main/java/com/example/worklens/exception/custom/DuplicateResourceException.java
+++ src/main/java/com/example/worklens/exception/custom/DuplicateResourceException.java
@@ -1,9 +1,10 @@
 package com.example.worklens.exception.custom;
 
 import com.example.worklens.exception.AppException;
+import org.springframework.http.HttpStatus;
 
 public class DuplicateResourceException extends AppException {
     public DuplicateResourceException(String message) {
-        super(message);
+        super(message, HttpStatus.CONFLICT);
     }
 }
--- src/main/java/com/example/worklens/exception/custom/ForbiddenAccessException.java
+++ src/main/java/com/example/worklens/exception/custom/ForbiddenAccessException.java
@@ -1,9 +1,10 @@
 package com.example.worklens.exception.custom;
 
 import com.example.worklens.exception.AppException;
+import org.springframework.http.HttpStatus;
 
 public class ForbiddenAccessException extends AppException {
     public ForbiddenAccessException(String message) {
-        super(message);
+        super(message, HttpStatus.FORBIDDEN);
     }
 }
