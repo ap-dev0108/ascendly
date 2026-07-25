67d882016d7f9f9dd9fb7fdd8b4a7478e80df7ec
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2026-07-25 19:24:33 +0545

    feat(services): Implementing logics on the Auth interface methods

--- src/main/java/com/example/worklens/services/AuthServices.java
+++ src/main/java/com/example/worklens/services/AuthServices.java
@@ -1,4 +1,27 @@
 package com.example.worklens.services;
 
-public class AuthServices {
+import com.example.worklens.dto.RegisterDTO;
+import com.example.worklens.entity.User;
+import com.example.worklens.repository.UserRepository;
+import com.example.worklens.services.interfaces.AuthImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServices implements AuthImpl {
+    private final UserRepository userRepository;
+
+    public AuthServices(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String RegisterUser(RegisterDTO registerDTO) {
+        var user = new User();
+
+        user.full_name = registerDTO.full_name;
+        user.email = registerDTO.email;
+        user.password = registerDTO.password;
+
+        userRepository.save(user);
+        return "Registration Done I guess";
+    }
 }
